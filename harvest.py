2045a9c42eaabdb626683a90227edd9691fc8377
Timothy McCallum <tpmccallum@users.noreply.github.com>
2015-07-05 08:39:15 +1000

    Update harvest.py

--- harvest.py
+++ harvest.py
@@ -3,6 +3,7 @@ import json
 import BeautifulSoup
 import re
 import os
+import subprocess
 
 url= 'http://data.gov.au/dataset/3fd356c6-0ad4-453e-82e9-03af582024c3/resource/3182591a-085a-465b-b8e5-6bfd934137f1/download/Localphotostories2009-2014-JSON.json'
 abcJson = urllib.urlopen(url).read()
